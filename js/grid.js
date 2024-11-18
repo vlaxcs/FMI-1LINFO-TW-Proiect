a1ad41a276687aa3190d8f4e9c8930af64e341a4
vlaxcs <minciunescuvlad.business@gmail.com>
2024-11-18 02:30:45 +0200

    JSON data to content

--- js/grid.js
+++ js/grid.js
@@ -1,40 +1,86 @@
-fetch('../data/bachelors.json')
-  .then(response => response.json())
-  .then(bachelorsDict => {
+let characters = [];
+let names = [];
 
-    const bachelors = bachelorsDict;
+async function AjaxBachelors() {
+    try {
+        const response = await fetch('../data/bachelors.json');
+        const bachelorsDict = await response.json();
 
-    for (let bachelor in bachelors) {
-      console.log(bachelor + ": " + bachelors[bachelor].lovedGift);
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
-
-console.log("Data from bachelors.json loaded!");
-
-fetch('../data/bachelorettes.json')
-  .then(response => response.json())
-  .then(bachelorettesDict => {
-
-    console.log(bachelorettesDict)
-    const bachelorettes = bachelorettesDict;
-
-    for (girl in bachelorettes){
-        console.log(girl, bachelorettes[girl]);
-        for (key in bachelorettes[girl]){
-            value = bachelorettes[girl][key];
-            console.log(value);
+        for (let key in bachelorsDict) {
+            characters.push(bachelorsDict[key]);
+            names.push(key);
         }
+
+        console.log("Data from bachelors.json loaded!");
+    } catch (error) {
+        console.error('Error fetching data from bachelors.json:', error);
     }
+}
 
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+async function AjaxBachelorettes() {
+    try {
+        const response = await fetch('../data/bachelorettes.json');
+        const bachelorettesDict = await response.json();
 
-console.log("Data from bachelorettes.json loaded!");
+        for (let key in bachelorettesDict) {
+            characters.push(bachelorettesDict[key]);
+            names.push(key);
+        }
 
+        console.log("Data from bachelorettes.json loaded!");
+    } catch (error) {
+        console.error('Error fetching data from bachelorettes.json:', error);
+    }
+}
 
-console.log(characters);
+async function AjaxNMCandidates() {
+    try {
+        const response = await fetch('../data/nonMarriageCandidates.json');
+        const nmcandidatesDict = await response.json();
+
+        for (let key in nmcandidatesDict) {
+            characters.push(nmcandidatesDict[key]);
+            names.push(key);
+        }
+
+        console.log("Data from nonMarriageCandidates.json loaded!");
+    } catch (error) {
+        console.error('Error fetching data from nonMarriageCandidates.json:', error);
+    }
+}
+
+async function main() {
+    await AjaxBachelors();
+    await AjaxBachelorettes();
+    await AjaxNMCandidates();
+
+    function populate() {
+        let chindex = 0;
+        let gridItem = document.querySelector('#grid-div');
+        const nameSpan = gridItem.querySelector('#name');
+        nameSpan.textContent = names[chindex];
+
+        gridItem = gridItem.nextElementSibling;
+        const birthdaySpan = gridItem.querySelector('#birthday');
+        birthdaySpan.textContent = characters[chindex].birthday;
+
+        const townSpan = gridItem.querySelector('#town');
+        townSpan.textContent = characters[chindex].livesIn;
+
+        const adressSpan = gridItem.querySelector('#address');
+        adressSpan.textContent = characters[chindex].address;
+
+        const marriageSpan = gridItem.quertSelector('#marriage');
+        if (characters[chindex]) 
+        {
+            gridItem.classList.add('marriageTrue');
+        }
+        else
+        {
+            gridItem.classList.add('marriageFalse');
+        } 
+    }
+    populate();
+}
+
+main();

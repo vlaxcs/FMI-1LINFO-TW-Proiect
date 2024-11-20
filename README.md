a0b76c3475f0b22b2718000e0f89ab5a6a67fc1e
vlaxcs <minciunescuvlad.business@gmail.com>
2024-11-20 03:21:17 +0200

    Added a loading screen, jumpy slimes, fancy keybinds

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# AJAX
+> sesiuni: e.g. login/logout (folosind Storage / fișier json)
+# JAVASCRIPT
+> inputuri funcționale (de exemplu: input de tip text/range/number/radio/checkbox, select, textarea)
+> validarea datelor dintr-un formular folosind expresii regulate
+Se continuă login.html
+
+> folosirea localStorage (să se pastreze în localStorage o colecție de elemente)
+Funcție care adaugă sau scoate un element din lista de favorite
+Trebuie să verifice dacă a fost deja băgat în listă, iar dacă este în listă se schimbă butonul de pe pagina de community.
+
+> folosirea proprietăților classList, target sau currentTarget
+Idk?
+
+> folosirea metodelor stopPropagation
+Idk?
+
--- js/script.js
+++ js/script.js
@@ -1,62 +1,229 @@
-window.onload = function() {
-function updateTime() {
-    const now = new Date();
+const loadingFacts = [
+    "You can visit Desert Calico!",
+    "Extra luck is variable, just watch TV!",
+    "Scarecrows are actually scaring crows!",
+    "You can unlock the Greenhouse!",
+    "The Community Center holds lots of secrets!",
+    "There are many mysteries in holes!",
+    "Garbage is surprisingly valuable!",
+    "Slimes smile in your farm!",
+    "Museum is not a work of art, Gunther neither. At least, not now."
+];
 
-    const daysOfWeek = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'];
-    let dayOfWeek = daysOfWeek[now.getDay()];
-    let day = now.getDate();
+window.onload = async function() {
+    
+    await updateFact();
+    function getRandomInt(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    async function updateFact() {
+        const fact = document.getElementById("loadingFact");
 
-    document.getElementById("cd").innerHTML = dayOfWeek + " " + day;
+        let randIndex = getRandomInt(0, loadingFacts.length - 1);
+        fact.innerHTML = loadingFacts[randIndex];
 
-    let hour = now.getHours();
-    let minutes = now.getMinutes();
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    }
 
-    let timeFormat = "am";
-    if (hour >= 12) {
-        if (hour > 12) {
-            hour = hour % 12;
+    async function loadingScreen() {
+        const ls = document.getElementsByClassName('loading')[0];
+        if (ls) {
+            ls.style.transition = 'opacity 0.2s ease';
+            ls.style.opacity = '0';
+
+            setTimeout(() => {
+                ls.remove();
+                const slimenone = document.getElementsByClassName('slime')[0];
+                slimenone.style.opacity = '1';
+                slimenone.style.left = String(getRandomInt(20, 60)) + 'vw';
+                slimenone.style.top = String(getRandomInt(20, 50)) + 'vw';
+            }, 200);
         }
     }
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    document.getElementById("ct").innerHTML = hour + ":" + minutes + " " + timeFormat;
-}
 
-updateTime();
-setInterval(updateTime, 1000);
-
-let count = 0;
-const button = document.getElementById("clickable");
-const counter = document.getElementById("count");
-
-function dropMoney() {
-
-    const money = document.createElement("img");
-    money.classList.add("money");
-
-    let location = new String();
-    location = window.location.href;
-    if (location.includes("build.html") || location.includes("community.html"))
-    {
-       money.src = "../assets/images/gold.png";
+    const counter = document.getElementById("count");
+    async function setCounter(){
+        if(localStorage.count === undefined)
+            {
+                localStorage.count = 0;
+            }
+        counter.innerHTML = "Click!";
     }
-    else
-    {
-        money.src = "assets/images/gold.png";
+
+    function updateTime() {
+        const now = new Date();
+
+        const daysOfWeek = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'];
+        let dayOfWeek = daysOfWeek[now.getDay()];
+        let day = now.getDate();
+
+        document.getElementById("cd").innerHTML = dayOfWeek + " " + day;
+
+        let hour = now.getHours();
+        let minutes = now.getMinutes();
+
+        let timeFormat = "am";
+        if (hour >= 12) {
+            if (hour > 12) {
+                hour = hour % 12;
+            }
+        }
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        document.getElementById("ct").innerHTML = hour + ":" + minutes + " " + timeFormat;
     }
-   
-    const randomX = Math.random() * window.innerWidth;
-    money.style.left = `${randomX}px`;
 
-    document.body.appendChild(money);
+    await updateTime();
+    await setCounter();
+    await loadingScreen();
 
-    setTimeout(() => {
-        money.remove();
-    }, 3000);
-}
+    setInterval(updateTime, 1000);
 
-button.addEventListener("click", function(){
-    dropMoney();
-    count++;
-    counter.innerHTML = count;
-});
+    function dropSomething(item) {
+
+        const money = document.createElement("img");
+        money.classList.add("money");
+
+        let location = new String();
+        location = window.location.href;
+
+        switch (item){
+            case "money":
+                if (location.includes("build.html") || location.includes("community.html") || location.includes("login.html"))
+                    {   
+                        money.src = "../assets/images/gold.png";
+                    }
+                    else
+                    {
+                        money.src = "assets/images/gold.png";
+                    }
+                    break;
+
+            case "star":
+                if (location.includes("build.html") || location.includes("community.html") || location.includes("login.html"))
+                    {   
+                        money.src = "../assets/images/star.png";
+                    }
+                    else
+                    {
+                        money.src = "assets/images/star.png";
+                    }
+                    break;
+
+            case "music":
+                if (location.includes("build.html") || location.includes("community.html") || location.includes("login.html"))
+                    {   
+                        money.src = "../assets/images/fluteblock.png";
+                    }
+                    else
+                    {
+                        money.src = "assets/images/fluteblock.png";
+                    }
+            }
+    
+        const randomX = Math.random() * window.innerWidth;
+        money.style.left = `${randomX}px`;
+
+        document.body.appendChild(money);
+
+        setTimeout(() => {
+            money.remove();
+        }, 3000);
+    }
+
+    const button = document.getElementById("clickable");
+    button.addEventListener("click", function(){
+        dropSomething("money");
+        localStorage.count++;
+        counter.innerHTML = Number(localStorage.count);
+    });
+
+    function toggleMenuState() {
+        const menu = document.getElementById('toggleMenu');
+        menu.checked = !menu.checked;
+    }
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key.toLowerCase() === 'm') {
+            toggleMenuState();
+            dropSomething("star");
+        }
+    });
+
+    function toggleAudio(){
+        const audio = document.getElementById("audiopt");
+        const audioAlert = document.getElementById("audioalert");
+        const style = window.getComputedStyle(audioAlert);
+        if (String(style.display) == "none")
+        {
+            audio.play();
+            audioAlert.style.display = "flex";
+            console.log(style.display);
+        }
+        else if (String(style.display) == "flex")
+        {
+            audio.pause();
+            audioAlert.style.display = "none";
+            console.log(style.display);
+        }
+
+    }
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key.toLowerCase() === 'p'){
+            toggleAudio();
+            dropSomething("music");
+        }
+    });
+
+    let lastInd = 0;
+    let flag = false;
+    function updateSlime(currentElement){
+        const elementStyles = window.getComputedStyle(currentElement);
+        let changeInd = getRandomInt(0, 2); 
+        if (flag != true){
+            while (changeInd == lastInd)
+            {
+                changeInd = getRandomInt(0, 2);
+                console.log(changeInd, lastInd);
+            }
+            console.log(changeInd, lastInd);
+            lastInd = changeInd;
+        }
+        else{
+            changeInd = 2;
+            flag = true;
+        }
+
+        let randomvaltop, randomvalleft, randomvalwidth, randomvalheigth;
+
+        switch (changeInd){
+            case 0:
+                randomvalwidth = String(getRandomInt(3, 6)) + "vw";
+                randomvalheigth = String(getRandomInt(3, 6)) + "vw";
+                currentElement.style.setProperty('height', randomvalwidth);
+                currentElement.style.setProperty('width', randomheigth);
+                break;
+            case 1:
+                randomvaltop = String(getRandomInt(10, 150)) + "vh";
+                randomvalleft = String(getRandomInt(10, 80)) + "vw";
+                currentElement.style.setProperty('top', randomvaltop);
+                currentElement.style.setProperty('left', randomvalleft);
+                break;
+            case 2:
+                randomvaltop = String(getRandomInt(10, 150)) + "vh";
+                randomvalleft = String(getRandomInt(10, 80)) + "vw";
+                randomvalwidth = String(getRandomInt(3, 6)) + "vw";
+                randomvalheigth = String(getRandomInt(3, 6)) + "vw";
+                currentElement.style.setProperty('top', randomvaltop);
+                currentElement.style.setProperty('left', randomvalleft);
+                currentElement.style.setProperty('height', randomvalwidth);
+                currentElement.style.setProperty('width', randomheigth);
+                break;
+            }
+    }
+
+    const slimeElement = document.getElementById("slime");
+    slimeElement.addEventListener("click", function(){
+        updateSlime(slimeElement);
+    });
 };
